31469c3a552606628b1f1ed35ea91feb800622eb
Gamu2059 <yamagami0711syo@gmail.com>
2020-11-10 14:38:32 +0900

    ゲームオーバー判定の実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,4 +17,55 @@ public class GameManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.DownArrow)) { }
     }
+
+    private bool IsGameOver(int[][] stageState)
+    {
+        // 空いている場所があればゲームオーバーにはならない
+        for (var i = 0; i < stageState.Length; i++)
+        {
+            for (var j = 0; j < stageState[i].Length; j++)
+            {
+                if (stageState[i][j] <= 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        // 合成可能なマスが一つでもあればゲームオーバーにはならない
+        for (var i = 0; i < stageState.Length; i++)
+        {
+            for (var j = 0; j < stageState[i].Length; j++)
+            {
+                var state = stageState[i][j];
+                var canMerge = false;
+                if (i > 0)
+                {
+                    canMerge |= state == stageState[i - 1][j];
+                }
+
+                if (i < stageState.Length - 1)
+                {
+                    canMerge |= state == stageState[i + 1][j];
+                }
+
+                if (j > 0)
+                {
+                    canMerge |= state == stageState[i][j - 1];
+                }
+
+                if (j < stageState[i].Length - 1)
+                {
+                    canMerge |= state == stageState[i][j + 1];
+                }
+
+                if (canMerge)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
