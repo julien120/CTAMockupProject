e9fd9ae1df39e4ae31584151a8ddcdace5ef38f4
Gamu2059 <yamagami0711syo@gmail.com>
2020-11-10 14:42:28 +0900

    リザルトへ遷移する処理を実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -67,5 +68,10 @@ public class GameManager : MonoBehaviour
         }
 
         return true;
+    }
+
+    private void LoadResultScene()
+    {
+        SceneManager.LoadScene("ResultScene");
     }
 }
