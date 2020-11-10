4b3ae53e3f7ccadd4ee5f027df8cab4345221be8
shun-shun123 <shunsuke15vb@gmail.com>
2020-11-10 13:55:06 +0900

    初期生成状態を実装

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -6,9 +6,11 @@ public class Cell : MonoBehaviour
     [SerializeField] private Image image;
 
     [SerializeField] private Text text;
-
+    
     public void SetText(int num)
     {
         text.text = num.ToString();
+        // TODO: 色が濃くなるロジックはもう少し考えた方がいいかも
+        image.color = Color.HSVToRGB( 25 / 255.0f, num * 2 / 255.0f, 100 / 255.0f);
     }
 }
