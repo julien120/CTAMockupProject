60a3743a74f8946662e0481deb464c2a1c3d5b6c
sotosaki <60338451+sTonozaki1211@users.noreply.github.com>
2020-11-14 15:23:12 +0900

    スコアをMVCに分割、他をPresenterに置いた

--- Assets/Scripts/InGameView.cs
+++ Assets/Scripts/InGameView.cs
@@ -6,35 +6,11 @@ public class InGameView : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
 
-    public event Action inputRihgt;
-    public event Action inputLeft;
-    public event Action inputUp;
-    public event Action inputDown;
-
-
     public void SetScore(int score)
     {
         scoreText.text = $"Score: {score}";
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            inputRihgt();
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            inputLeft();
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            inputUp();
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            inputDown();
-        }
-
-
-    }
 
 
 }
