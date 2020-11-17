a96d0487696abba44b873e754bc26e5d5ad1e6e9
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-11-18 04:12:35 +0900

    add SceneController

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController _instance;
+
+    //_instanceに一意のインスタンを格納し、これのみ参照する構造
+    public static SceneController Instance
+    {
+
+        get
+        {
+            if (_instance == null)
+            {
+
+                GameObject single = new GameObject();
+                //_instanceに格納されてる値を管理する
+                _instance = single.AddComponent<SceneController>();
+                //scene跨いでもインスタンスが残るのでnull処理に行かない
+                DontDestroyOnLoad(_instance);
+
+            }
+            return _instance;
+
+        }
+    }
+
+    public void LoadResultScene()
+    {
+        SceneManager.LoadScene(SceneName.ResultScene);
+    }
+
+    public void LoadInGameScene()
+    {
+        SceneManager.LoadScene(SceneName.InGameScene);
+    }
+}
--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -8,7 +8,7 @@ public class Title : MonoBehaviour
 
     public void LoadInGameScene()
     {
-        SceneManager.LoadScene(SceneController.InGameScene);
+        SceneController.Instance.LoadInGameScene();
     }
 
 }
