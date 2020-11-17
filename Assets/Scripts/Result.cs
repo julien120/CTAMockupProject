af100d7eb36049c0ee89df9ff697f60e60a67403
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-11-18 00:32:24 +0900

    fix namingProtcol

--- Assets/Scripts/Result.cs
+++ Assets/Scripts/Result.cs
@@ -9,7 +9,7 @@ public class Result : MonoBehaviour
 
     private void Start()
     {
-        resultText.text = PlayerPrefs.GetInt("SCORE", 0).ToString();
+        resultText.text = PlayerPrefs.GetInt(InGamePresenter.ScoreData, 0).ToString();
     }
 
     public void OnClickRetryButton()
