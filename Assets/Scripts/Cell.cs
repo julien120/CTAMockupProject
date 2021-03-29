a615bdef4a2f65fa52b902f29e512de32056668a
M-Yuki-Hasuo <huyuda1220@gmail.com>
2021-03-29 22:44:47 +0900

    add IMG

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -5,12 +5,20 @@ public class Cell : MonoBehaviour
 {
     [SerializeField] private Image image;
     [SerializeField] private Text text;
+    private string colorString = "#E86993";
 
     public void SetText(int num)
     {
         text.text = num.ToString();
         gameObject.SetActive(num != 0);
         // TODO: 色が濃くなるロジックを考える
-        image.color = Color.HSVToRGB(1f, 1f, 1f);
+        Color color = default(Color);
+        ColorUtility.TryParseHtmlString(colorString, out color);
+        image.color = color;
+
+
+
     }
+
+
 }
